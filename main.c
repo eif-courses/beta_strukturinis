37a85e4ed02ad780b9bd9b6dca889e65514531c3
Marius Gegoevskis <m.gzegozevskis@eif.viko.lt>
2018-09-23 23:12:52 +0300

    simple syntax

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Namas{
+    int nr;
+    char gatve[44];
+    char *miestas;
+};
 
 int main(int argc, char const *argv[])
 {
-    printf("hello");
+
+    struct Namas darbas;
+    darbas.miestas = malloc(50);
+    strcpy(darbas.miestas, "Vilnius");
+    darbas.nr = 15;
+    strcpy(darbas.gatve, "Jasinskio g.");
+    printf("output: %s %d, %s", darbas.gatve, darbas.nr, darbas.miestas);
+    free(darbas.miestas);
     return 0;
 }
